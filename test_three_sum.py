f2fffeb2078e3186ec1243270f531d6fa9f5801d
joni <joni@haggei.de>
2022-10-15 13:58:29 +0200

    Redo three_sum to make it way shorter. It is still too slow (308/311 test cases)

--- test_three_sum.py
+++ test_three_sum.py
@@ -5,33 +5,21 @@ from typing import List
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        two_sum: dict[tuple[int, int], int] = {}
+        valid_three_sums = set()
         for a in range(len(nums)):
             for b in range(a + 1, len(nums)):
-                two_sum[(a, b)] = nums[a] + nums[b]
-
-        result_indices = set()
-        for c in range(len(nums)):
-            for keys, val in two_sum.items():
-                if c in keys:
-                    continue
-                if val + nums[c] == 0:
-                    result_indices.add((keys[0], keys[1], c))
-
-        result = set()
-
-        for i in result_indices:
-            zero_sum = [nums[i[0]], nums[i[1]], nums[i[2]]]
-            zero_sum.sort()
-            result.add(tuple(zero_sum))
-
-        return [list(zero_sum) for zero_sum in result]
+                for c in range(b + 1, len(nums)):
+                    if nums[a] + nums[b] + nums[c] == 0:
+                        zero_sum = [nums[a], nums[b], nums[c]]
+                        zero_sum.sort()
+                        valid_three_sums.add(tuple(zero_sum))
+        return list(list(zero_sum) for zero_sum in valid_three_sums)
 
 
 @pytest.mark.parametrize(
     "nums,expected",
     [
-        ([-1, 0, 1, 2, -1, -4], [[-1, 0, 1], [-1, -1, 2]]),
+        ([-1, 0, 1, 2, -1, -4], [[-1, -1, 2], [-1, 0, 1]]),
         ([0, 1, 1], []),
         ([0, 0, 0], [[0, 0, 0]]),
     ],
