f61479a4b28662db7c44dc45a98aa03f686cb735
joni <joni@haggei.de>
2022-10-23 16:27:00 +0200

    Add reverse linked list

---
+++ test_reverse_linked_list.py
@@ -0,0 +1,41 @@
+# https://leetcode.com/problems/reverse-linked-list/
+
+import pytest
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def __repr__(self):
+        return f"{self.val}, {self.next}"
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        current = head
+        previous = None
+        while current:
+            next = current.next
+            current.next = previous
+            previous = current
+            current = next
+        return previous
+
+
+@pytest.mark.parametrize(
+    "head,expected",
+    [
+        (
+            ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5))))),
+            ListNode(5, ListNode(4, ListNode(3, ListNode(2, ListNode(1))))),
+        ),
+        (ListNode(1, ListNode(2)), ListNode(2, ListNode(1))),
+    ],
+)
+def test_topKFrequent(head, expected):
+    sol = Solution().reverseList(head)
+    assert str(expected) == str(sol)
